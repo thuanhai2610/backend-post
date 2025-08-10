1842e20682b6a34b86177ca29a6249a5cd0270f2
thuanhai2610 <111416711+thuanhai2610@users.noreply.github.com>
2025-08-10 21:54:24 +0700

    Update user.schema.ts

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -5,7 +5,7 @@ export class User{
     @Prop({required: true}) email: string;
     @Prop({required: true}) password: string;
     @Prop({default: 'user'}) role: string;
-    @Prop({required: true}) isVerified: boolean;
+    @Prop({required: true, default: true}) isVerified: boolean;
 
 }
 export const UserSchema = SchemaFactory.createForClass(User);
